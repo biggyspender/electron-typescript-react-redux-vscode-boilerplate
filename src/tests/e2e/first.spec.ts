be4b1da9183d0c20d907c1d4f5d475a7c57dc36f
Chris Sperry <christophermartinsperry@gmail.com>
2018-12-07 03:49:30 +0000

    e2e stuff

--- src/tests/e2e/first.spec.ts
+++ src/tests/e2e/first.spec.ts
@@ -1,7 +1,7 @@
 import { Application } from "spectron";
 import * as path from "path"
 describe("first test", function () {
-    jest.setTimeout(20000);
+    jest.setTimeout(10000);
     let app: Application;
     beforeEach(function () {
         const projectRoot = path.join(__dirname, '..', '..', '..');
@@ -15,16 +15,14 @@ describe("first test", function () {
         return (app && app.stop()) || Promise.resolve();
     })
     test("test1", async function () {
-        await app.client.waitForExist('#counter-navlink', 20000)
+        await app.client.waitForExist('#counter-navlink')
         await app.client.click('#counter-navlink')
         const valStr = await app.client.getText('#counter-count')
-        console.log(valStr)
         const val = parseInt(valStr)
         expect(val).not.toBeNaN()
         await app.client.click('#increment-button')
         const newValStr = await app.client.getText('#counter-count')
         const newVal = parseInt(newValStr)
-        console.log(`old : ${val}, new : ${newVal}`)
         expect(newVal).toBe(val + 1)
     })
 
